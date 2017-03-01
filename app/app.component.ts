eed09f29f0c1ae5757d235efb3947c37ac15e54b
Ognjen Jevremovic <jevremovic.ognjen@gmail.com>
2017-03-01 13:52:03 +0100

    Angular root module and component

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId : module.id,
+    templateUrl : './app.component.html',
+    selector : 'ng-app'
+})
+export class AppComponent { }
