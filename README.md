adde220ac12229fdd586346d67a73b5665a275be
Ognjen Jevremovic <jevremovic.ognjen@gmail.com>
2017-03-01 13:51:45 +0100

    Angular 2 setup

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# angular2_boilerplate
-Angular 2 starter project using SystemJS
+# Angular 2 Boilerplate
+Angular 2 Boilerplate with SystemJS and minimal dependancies.
+
+### Clone the repo
+cd into directory
+
+### Install dependancies
+To install dependancies.
+```sh
+npm install
+```
+Typing definitions and all the vendor libraries (Angular 2 dependancy libraries and Angular 2 modules) will be created for you, after you run npm install.
+
+### Start the server
+```sh
+npm start
+```
+To compile TypeScript files, start the live-server and watch the files for change.
---
+++ systemjs.config.js
@@ -0,0 +1,33 @@
+(function(global) {
+    'use strict';
+
+    System.config({
+        paths: {
+            'vendor:'   : 'app/vendor/',
+            '@angular:' : 'vendor:@angular/'
+        },
+        map: {
+            app: 'app',
+
+            '@angular/core'     :   '@angular:core/bundles/core.umd.min.js',
+            '@angular/common'   :   '@angular:common/bundles/common.umd.min.js',
+            '@angular/compiler' :   '@angular:compiler/bundles/compiler.umd.min.js',
+            '@angular/http'     :   '@angular:http/bundles/http.umd.min.js',
+            '@angular/forms'    :   '@angular:forms/bundles/forms.umd.min.js',
+            '@angular/router'   :   '@angular:router/bundles/router.umd.min.js',
+            '@angular/platform-browser' :   '@angular:platform-browser/bundles/platform-browser.umd.min.js',
+            '@angular/platform-browser-dynamic' :   '@angular:platform-browser-dynamic/bundles/platform-browser-dynamic.umd.min.js',
+
+            'rxjs'  : 'vendor:rxjs'
+        },
+        packages: {
+            app: {
+                main : './main.js',
+                defaultExtension : 'js'
+            },
+            rxjs : {
+                defaultExtension : 'js'
+            }
+        }
+    });
+})(this);
